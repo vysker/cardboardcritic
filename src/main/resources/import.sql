1c6f47ca6c111cc8e882f12030919c7388048596
Dick Visser <info@vysk.nl>
2022-07-08 18:29:07 +0200

    Remove ids from dummy import.sql; it caused issues

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,35 +1,35 @@
 insert into game (
-	id, name, short_description, description, designer, release_date, score, recommended, slug)
+	name, short_description, description, designer, release_date, score, recommended, slug)
 	values
-	(1, 'Anachrony', 'Not long description', 'Elaborate description', 'David Turczi', '2017-01-01', 80, 70, 'anachrony'),
-	(2, 'Gloomhaven', 'Very brief', 'Quite long', 'Isaac Childres', '2017-01-01', 89, 80, 'gloomhaven')
+	('Anachrony', 'Not long description', 'Elaborate description', 'David Turczi', '2017-01-01', 80, 70, 'anachrony'),
+	('Gloomhaven', 'Very brief', 'Quite long', 'Isaac Childres', '2017-01-01', 89, 80, 'gloomhaven')
 	;
 
 insert into critic (
-  id, name)
+  name)
   values
-  (1, 'John Doe'),
-  (2, 'Jane Bane'),
-  (3, 'Heckler Barrymore')
+  ('John Doe'),
+  ('Jane Bane'),
+  ('Heckler Barrymore')
   ;
 
 insert into outlet (
-  id, name, website)
+  name, website)
   values
-  (1, 'Ars Technica', 'https://arstechnica.com/'),
-  (2, 'Eurogamer', 'https://eurogamer.net')
+  ('Ars Technica', 'https://arstechnica.com/'),
+  ('Eurogamer', 'https://eurogamer.net')
   ;
 
 insert into review (
-  id, game_id, critic_id, outlet_id, summary, score, recommended, url)
+  game_id, critic_id, outlet_id, summary, score, recommended, url)
   values
-  (1, 1, 1, 1, 'Lorem ipsum dolor sit amet', 95, true, 'https://arstechnica.com/gaming/2017/04/gloomhaven-review-2017s-biggest-board-game-is-astoundingly-good'),
-  (2, 1, 2, 1, 'Lorem ipsum dolor sit amet', 90, true, null),
-  (3, 1, 3, 2, 'Lorem ipsum dolor sit amet', 50, false, null)
+  (1, 1, 1, 'Lorem ipsum dolor sit amet', 95, true, 'https://arstechnica.com/gaming/2017/04/gloomhaven-review-2017s-biggest-board-game-is-astoundingly-good'),
+  (1, 2, 1, 'Lorem ipsum dolor sit amet', 90, true, null),
+  (1, 3, 2, 'Lorem ipsum dolor sit amet', 50, false, null)
   ;
 
 insert into raw_review (
-  id, game, critic, outlet, recommended, score, summary, title, content, date, url, processed)
+  game, critic, outlet, recommended, score, summary, title, content, date, url, processed)
   values
-  (1, 'Abc', 'Herb Bergerstern', null, false, 0, null, 'We like Anachrony', 'Lorem ipsum', '2017-01-01', null, false)
+  ('Abc', 'Herb Bergerstern', null, false, 0, null, 'We like Anachrony', 'Lorem ipsum', '2017-01-01', null, false)
   ;
