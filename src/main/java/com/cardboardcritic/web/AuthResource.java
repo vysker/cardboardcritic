0ddfbc690bbe8842f567180536cbeb6595e63e13
Dick Visser <info@vysk.nl>
2022-07-19 16:11:51 +0200

    Add logout for form based auth

--- src/main/java/com/cardboardcritic/web/AuthResource.java
+++ src/main/java/com/cardboardcritic/web/AuthResource.java
@@ -10,7 +10,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 import java.net.URI;
 
@@ -29,5 +28,15 @@ public class AuthResource {
     @PermitAll
     public Uni<TemplateInstance> login() {
         return Uni.createFrom().item(Templates.login());
+    }
+
+    @GET
+    @Path("logout")
+    @Produces(MediaType.TEXT_HTML)
+    @PermitAll
+    public Response logout() {
+        return Response.seeOther(URI.create("/"))
+                .header("Set-Cookie", "quarkus-credential=; path=/")
+                .build();
     }
 }
