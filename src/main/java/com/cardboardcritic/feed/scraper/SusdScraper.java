16089034398399f93bf3b01cc984798a92852e8e
Dick Visser <info@vysk.nl>
2023-06-14 10:48:10 +0200

    Make SusdScraper more null-safe

--- src/main/java/com/cardboardcritic/feed/scraper/SusdScraper.java
+++ src/main/java/com/cardboardcritic/feed/scraper/SusdScraper.java
@@ -2,10 +2,10 @@ package com.cardboardcritic.feed.scraper;
 
 import com.cardboardcritic.db.entity.RawReview;
 import com.cardboardcritic.feed.ScrapeException;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-
+import com.cardboardcritic.util.ScraperUtil;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.jsoup.nodes.Document;
+
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
@@ -14,16 +14,11 @@ public class SusdScraper extends ArticleScraper {
 
     @Override
     public RawReview getReview(String articleUrl, Document document) throws ScrapeException {
-        final String date = document.select("span.entry-date").stream()
-                .findFirst()
-                .map(Element::text)
+        final String date = ScraperUtil.selectText(document, "span.entry-date")
                 .map(stringDate -> DateTimeFormatter.ofPattern("MMMM d, yyyy").parse(stringDate))
                 .map(Objects::toString)
                 .orElse(null);
-        final String title = document.select("header h1.entry-title").stream()
-                .findFirst()
-                .map(Element::text)
-                .orElse(null);
+        final String title = ScraperUtil.selectText(document, "header h1.entry-title").orElse(null);
 
         return new RawReview()
                 .setDate(date)
